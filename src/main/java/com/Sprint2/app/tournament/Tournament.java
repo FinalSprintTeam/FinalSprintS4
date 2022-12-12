f5a820b3c2da1f14392352dad50d879a4293c0c9
Tarsolan <aridgeley@msn.com>
2022-12-12 09:10:53 -0330

    Added Dockerfile

--- src/main/java/com/Sprint2/app/tournament/Tournament.java
+++ src/main/java/com/Sprint2/app/tournament/Tournament.java
@@ -18,16 +18,20 @@ public class Tournament {
     @Id
     @SequenceGenerator(name = "tournament_sequence", sequenceName = "tournament_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tournament_sequence")
-
     private Long id;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "start_date")
     private Date startDate;
+
     @Column(name = "end_date")
     private Date endDate;
+
     @Column(name = "location")
     private String location;
+
     @Column(name = "entry_fee")
     private double entryFee;
 
